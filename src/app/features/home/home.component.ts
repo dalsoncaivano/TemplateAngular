d12e0d4b1232cfefc5f74a7e137588170a80520f
Dalson <dalson.anjos.caivano@gmail.com>
2025-11-18 13:50:28 -0300

    Inicio da construção da homepage

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  ngOnInit(): void {}
+export class HomeComponent implements OnInit, OnDestroy {
+  ngOnInit(): void {
+    document.querySelector('.page-body')?.classList.add('page-body--flush');
+  }
+
+  ngOnDestroy(): void {
+    document.querySelector('.page-body')?.classList.remove('page-body--flush');
+  }
 }
